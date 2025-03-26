ce8b06d9e3b2b44fbab165db44b7fe18fa476512
botao <botao@codeleap.cn>
2025-03-26 12:58:07 +0800

    初始化

---
+++ src/main/java/cn/jiangbt/demo/DemoController.java
@@ -0,0 +1,12 @@
+package cn.jiangbt.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
